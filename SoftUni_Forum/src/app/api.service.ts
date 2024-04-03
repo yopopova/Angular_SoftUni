a9066b2b20b2e99186af076d50dd677dcfb0eca3
Yoanna Popova <yoanna.dp@gmail.com>
2024-04-03 10:47:40 +0300

    Get data from the server.

---
+++ SoftUni_Forum/src/app/api.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment.development';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor(private http: HttpClient) { }
+
+  getThemes() {
+    const { apiUrl } = environment;
+
+    return this.http.get(`${apiUrl}/themes`);
+  }
+
+  // We have limit to the posts we should show on the view.
+  getPosts(limit?: number) {
+
+  }
+}
--- SoftUni_Forum/src/app/app.module.ts
+++ SoftUni_Forum/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
@@ -20,7 +21,8 @@ import { PostsListComponent } from './main/posts-list/posts-list.component';
     BrowserModule,
     AppRoutingModule,
     CoreModule,
-    SharedModule
+    SharedModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
