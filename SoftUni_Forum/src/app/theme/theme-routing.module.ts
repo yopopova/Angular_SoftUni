76f477a446b10d07b30dc4c0241d513a09d57575
Yoanna Popova <yoanna.dp@gmail.com>
2024-04-18 11:15:41 +0300

    Reorder app component and set theme routing up.

--- SoftUni_Forum/src/app/theme/theme-routing.module.ts
+++ SoftUni_Forum/src/app/theme/theme-routing.module.ts
@@ -1,9 +1,16 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { MainComponent } from "./main/main.component";
+import { AddThemeComponent } from "./add-theme/add-theme.component";
+import { CurrentThemeComponent } from "./current-theme/current-theme.component";
 
 const routes: Routes = [
-    { path: 'themes', component: MainComponent }
+    // Like this we add children routes to our theme module.
+    { path: 'themes', children: [
+        { path: '', pathMatch: 'full', component: MainComponent },
+        { path: ':themeId', component: CurrentThemeComponent }
+    ]},
+    { path: 'add-theme', component: AddThemeComponent }
 ];
 
 @NgModule({
