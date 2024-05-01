dc2f9d789d9f03434eb5b64d4d005c1dbb62d957
Yoanna Popova <yoanna.dp@gmail.com>
2024-05-01 16:46:15 +0300

    Make register form error handling and set up login form submit button.

--- SoftUni_Forum/src/app/user/register/register.component.ts
+++ SoftUni_Forum/src/app/user/register/register.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { EMAIL_DOMAINS } from 'src/app/constants';
 import { emailValidator } from 'src/app/shared/utils/email-validator';
+import { matchPasswordsValidator } from 'src/app/shared/utils/match-passwords-validator';
 
 @Component({
   selector: 'app-register',
@@ -18,8 +19,15 @@ export class RegisterComponent {
     passGroup: this.fb.group({
       password: ['', [Validators.required]],
       rePassword: ['', [Validators.required]],
+    }, {
+     validators: [matchPasswordsValidator('password', 'rePassword')],   
     })
   });
+
+  // passGroup === form.get('passGroup')
+  get passGroup() {
+    return this.form.get('passGroup');
+  }
 
   constructor(private fb: FormBuilder) {}
 
