85230dffdcb39640b90d7895b9ed1619eb0e50ff
Yoanna Popova <yoanna.dp@gmail.com>
2024-05-03 13:32:07 +0300

    Optimize login register, and logout functionality.

--- SoftUni_Forum/src/app/user/user.service.ts
+++ SoftUni_Forum/src/app/user/user.service.ts
@@ -1,12 +1,18 @@
 import { Injectable } from '@angular/core';
 import { UserForAuth } from '../types/user';
 import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 
 export class UserService {
+  // user$$ = subject
+  private user$$ = new BehaviorSubject<UserForAuth | undefined>(undefined);
+  // user$ = observable
+  private user$ = this.user$$.asObservable();
+
   user: UserForAuth | undefined;
   USER_KEY = '[user]';
 
@@ -14,26 +20,29 @@ export class UserService {
     return !!this.user;
   }
 
-  constructor(private http: HttpClient) {
-    try {
-      const lsUser = localStorage.getItem(this.USER_KEY) || '';
-      this.user = JSON.parse(lsUser);
-
-    } catch (error) {
-      this.user = undefined;
-    }
-  }
+  constructor(private http: HttpClient) {}
 
   login(email: string, password: string) {
-    return this.http.post<UserForAuth>('/api/login', { email, password });
+    return this.http.post<UserForAuth>('/api/login', { email, password }).pipe(
+      tap((user) => {
+        this.user$$.next(user);
+      })
+    );
   }
 
   register(username: string, email: string, tel: string, password: string, rePassword: string) {
-    return this.http.post<UserForAuth>('/api/register', {username, email, tel, password, rePassword});
+    return this.http.post<UserForAuth>('/api/register', {username, email, tel, password, rePassword}).pipe(
+      tap((user) => {
+        this.user$$.next(user);
+      })
+    );
   }
 
   logout() {
-    this.user = undefined;
-    localStorage.removeItem(this.USER_KEY);
+    return this.http.post('/api/logout', {}).pipe(
+      tap((user) => {
+        this.user$$.next(undefined);
+      })
+    );
   }
 }
