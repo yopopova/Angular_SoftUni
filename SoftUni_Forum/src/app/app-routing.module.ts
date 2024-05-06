2dc2ce407684b8a39df0a80afc3e4d870c85891a
Yoanna Popova <yoanna.dp@gmail.com>
2024-05-06 13:43:13 +0300

    Create error component and add it to the router.

--- SoftUni_Forum/src/app/app-routing.module.ts
+++ SoftUni_Forum/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { ErrorComponent } from './core/error/error.component';
 
 // With pathMatch: 'full' we check for white spases in the router
 // If we don't use 'redirectTo', our URL will be dirty; it won't be 404, but the thing we or someone write
@@ -10,6 +11,7 @@ const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: '/home' },
   { path: 'home', component: HomeComponent},
   { path: 'auth', loadChildren: () => import('./user/user.module').then(m => m.UserModule) }, // Lazy loading example; we remove the UserModule from AppModule to add it here
+  { path: 'error', component: ErrorComponent },
   { path: '**', redirectTo: '/404' },
   { path: '404', component: NotFoundComponent}
 ];
