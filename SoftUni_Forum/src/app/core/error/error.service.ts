39f169e3fcd12be2dcaa50a3ba3245915ba9ddf7
Yoanna Popova <yoanna.dp@gmail.com>
2024-05-06 14:25:06 +0300

    Set error handling up.

--- SoftUni_Forum/src/app/core/error/error.service.ts
+++ SoftUni_Forum/src/app/core/error/error.service.ts
@@ -1,10 +1,17 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 
 export class ErrorService {
+  private apiError$$ = new BehaviorSubject(null);
+  public apiError$ = this.apiError$$.asObservable();
 
-  constructor() { }
+  constructor() {}
+
+  setError(error: any): void {
+    this.apiError$$.next(error);
+  }
 }
