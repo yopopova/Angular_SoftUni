b8d8b4b708279146fce78b0d29791548173a7311
Yoanna Popova <yoanna.dp@gmail.com>
2024-11-06 09:37:13 +0200

    Add solution to the third task.

---
+++ TypeScript_Playground_Lab/03-people/src/index.ts
@@ -0,0 +1,32 @@
+import { Employee } from "./Employee";
+
+export class Junior extends Employee {
+    constructor(name: string, age: number) {
+        super(name, age);
+        this.tasks.push(" is working on a simple task.");
+    }
+}
+
+export class Senior extends Employee {
+    constructor(name: string, age: number) {
+        super(name, age);
+        this.tasks.push(" is working on a complicated task.");
+        this.tasks.push(" is taking time off work.");
+        this.tasks.push(" is supervising junior workers.");
+    }
+}
+
+export class Manager extends Employee {
+    public divident: number;
+
+    constructor(name: string, age: number) {
+        super(name, age);
+        this.divident = 0;
+        this.tasks.push(" scheduled a meeting.");
+        this.tasks.push(" is preparing a quarterly report.");
+    }
+
+    public getSalary(): number {
+        return this.salary + this.divident;
+    }
+}
